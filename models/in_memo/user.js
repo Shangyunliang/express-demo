76ed5c9933ffe00f412c530d7cb76064d3a5a2e1
ShangYunliang <1071332303@qq.com>
2017-10-29 01:13:27 +0800

    完成了获取用户信息,更改用户信息的操作

---
+++ models/in_memo/user.js
@@ -0,0 +1,41 @@
+let USER_ID_INIT =  10000;
+users = [];
+
+class User {
+  constructor (params) {
+    if(!params.name || !params.age) throw new Error('name and age required when create user')
+    this.name = params.name;
+    this.age = params.age;
+    this._id = USER_ID_INIT ++;
+  }
+}
+
+async function createANewUser (params) {
+  const user = new User(params);
+  users.push(user);
+  return user
+}
+
+
+async function getUsers (params) {
+  return users;
+}
+
+async function getUserById (userId) {
+  return users.find(u=>u._id === userId);
+}
+
+async function updateUserById (userId, update) {
+  const user = users.find(u=>u._id === userId)
+  if(update.name) user.name = update.name;
+  if(update.age) user.age = update.age;
+}
+
+
+module.exports = {
+  model: User,
+  createANewUser,
+  getUsers,
+  getUserById,
+  updateUserById,
+};
